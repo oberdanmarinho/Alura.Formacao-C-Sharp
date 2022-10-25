2bba111be6c77becd5f08ba64bc329a74beb93b2
Oberdan Marinho <oberdanmarinho@gmail.com>
2022-10-25 00:39:08 -0300

    Meu primeiro Commit

---
+++ (R) C# Parte 2 Entendendo a Orientacao a Objetos/01.Nossa primeira classe/ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 1399;
+            contaDaGabriela.numero = 55627;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine(contaDaGabriela.titular);
+            Console.WriteLine(contaDaGabriela.agencia);
+            Console.WriteLine(contaDaGabriela.numero);
+            Console.WriteLine(contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200;
+            Console.WriteLine(contaDaGabriela.saldo);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ (R) C# Parte 2 Entendendo a Orientacao a Objetos/02.Tipos/03-ByteBank/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 865332;
+
+            ContaCorrente contaDaGabrielaCosta = new ContaCorrente();
+
+            contaDaGabrielaCosta.titular = "Gabriela";
+            contaDaGabrielaCosta.agencia = 863;
+            contaDaGabrielaCosta.numero = 865332;
+
+            Console.WriteLine("Igualdade de tipo de referencia = " + (contaDaGabriela == contaDaGabrielaCosta));
+
+            int idade = 27;
+            int idadeDaGabriela = 27;
+
+            Console.WriteLine("Igualdade de tipo de valor = " + (idade == idadeDaGabriela));
+
+            contaDaGabriela = contaDaGabrielaCosta;
+            Console.WriteLine(contaDaGabriela == contaDaGabrielaCosta);
+
+            contaDaGabriela.saldo = 300;
+
+            Console.WriteLine(contaDaGabriela.saldo);
+            Console.WriteLine(contaDaGabrielaCosta.saldo);
+
+            if (contaDaGabriela.saldo >= 100)
+            {
+                contaDaGabriela.saldo -= 100;
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
