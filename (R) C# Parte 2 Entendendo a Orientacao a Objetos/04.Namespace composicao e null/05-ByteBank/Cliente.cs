4ea3ea2e5bc2acbb89e3f67f7647acaf6004e498
Oberdan Marinho <oberdanmarinho@gmail.com>
2022-10-27 00:28:45 -0300

    Aula 04

---
+++ (R) C# Parte 2 Entendendo a Orientacao a Objetos/04.Namespace composicao e null/05-ByteBank/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _05_ByteBank
+{
+    public class Cliente
+    {
+        public string nome;
+        public string cpf;
+        public string profissao;
+
+
+    }
+}
+
