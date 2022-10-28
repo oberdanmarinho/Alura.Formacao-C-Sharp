13023ba333173208de77514db8abce7afa36c857
Oberdan Marinho <oberdanmarinho@gmail.com>
2022-10-28 00:44:58 -0300

    Conclusão Aula 04

--- (R) C# Parte 2 Entendendo a Orientacao a Objetos/04.Namespace composicao e null/05-ByteBank/Program.cs
+++ (R) C# Parte 2 Entendendo a Orientacao a Objetos/04.Namespace composicao e null/05-ByteBank/Program.cs
@@ -6,7 +6,30 @@ namespace _05_ByteBank
     {
         static void Main(string[] args)
         {
-            Cliente gabriela = new Cliente();
+            //Cliente gabriela = new Cliente();
+
+            //gabriela.nome = "Gabriela";
+            //gabriela.profissao = "Desenvolvedora C#";
+            //gabriela.cpf = "454.445.498-03";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            //conta.titular = gabriela;
+            //conta.titular = new Cliente();
+            conta.titular.nome = "Gabriela Costa";
+            conta.titular.cpf = "454.445.498-03";
+            conta.titular.profissao = "Desenvolvedora c#";
+
+            conta.saldo = 500;
+            conta.agencia = 454;
+            conta.numero = 33392;
+
+
+            // Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+            Console.WriteLine(conta.titular.cpf);
+            Console.WriteLine(conta.titular.profissao);
+            Console.ReadLine();
         }
     }
 }
