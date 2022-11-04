157d3554c2398419252092042924999ef631e33a
Oberdan Marinho <oberdanmarinho@gmail.com>
2022-11-04 00:34:36 -0300

    Aula 05

---
+++ (R) C# Parte 2 Entendendo a Orientacao a Objetos/05.Propriedades/06-ByteBank/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _06_ByteBank
+{
+    public class Cliente
+    {
+        public string nome;
+        public string cpf;
+        public string profissao;
+
+
+    }
+}
+
---
+++ (R) C# Parte 2 Entendendo a Orientacao a Objetos/05.Propriedades/06-ByteBank/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.DefinirSaldo(-10);
+            
+            Console.WriteLine(conta.ObterSaldo());
+
+
+            Console.ReadLine();
+        }
+    }
+}
